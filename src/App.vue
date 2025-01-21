07ac82c99a5aac9ffc38665059a7ff16baa247fd
oschwill <o.schwill@web.de>
2025-01-21 15:50:54 +0100

    create some Components

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <script setup>
 import NavBar from '@/components/Navbar.vue';
+import Hero from '@/components/Hero.vue';
+import HomeCards from '@/components/HomeCards.vue';
+import JobListings from '@/components/JobListings.vue';
 </script>
 
 <template>
   <NavBar />
+  <Hero />
+  <HomeCards />
+  <JobListings />
 </template>
